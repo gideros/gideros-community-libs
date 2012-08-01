c6fbbea206c84b6e30f5405e580ccc1fed39cc91
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-08-02 01:48:11 +0530

    Make pull branches explicit in sync.sh.

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 while read repo
 do
   directory=`basename $repo | sed s/\.git$//`
-  (cat .gitmodules | grep $repo && (cd vendor/$directory && git pull)) ||
+  (cat .gitmodules | grep $repo && (cd vendor/$directory && git pull origin master)) ||
     git submodule add $repo vendor/$directory
 done < libraries.txt
